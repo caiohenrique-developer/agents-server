44bcb73c7a6d09af3c956e9b43401b972d8b1335
caiohenrique-developer <caiohenrique.developer@gmail.com>
2025-07-11 14:18:07 -0300

    createRoomsRoute

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
-import { fastifyCors } from '@fastify/cors'
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
 import {
-  serializerCompiler,
-  validatorCompiler,
-  type ZodTypeProvider,
-} from 'fastify-type-provider-zod'
-import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+	type ZodTypeProvider,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+import { createRoomsRoute } from "./http/routes/create-rooms.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.register(fastifyCors, { origin: 'http://localhost:5173' })
+app.register(fastifyCors, { origin: "http://localhost:5173" });
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
+app.register(createRoomsRoute);
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log(`🖥️  Server is running on port ${env.PORT} !!!`)
-})
+	console.log(`🖥️  Server is running on port ${env.PORT} !!!`);
+});
